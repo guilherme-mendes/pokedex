fbd98faa80300388ef09270e06b558e774034c73
guilherme-mendes <guimendesp12@gmail.com>
2018-10-22 21:35:49 -0300

    commit inicial, adição da classe Pokemon e Treinador

---
+++ src/model/Treinador.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Treinador {
+    private String nome;
+    private String usuario;
+    private String senha;
+    private int genero;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getGenero() {
+        return genero;
+    }
+
+    public void setGenero(int genero) {
+        this.genero = genero;
+    }
+    
+    
+}
