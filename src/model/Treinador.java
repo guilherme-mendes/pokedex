c298f944dfef3dc7a1f49f9d0971dabc9e3c66ad
guilherme-mendes <guimendesp12@gmail.com>
2018-11-05 13:54:55 -0200

    Alteração da classe Treinador e adição dos icones que serão utilizados

--- src/model/Treinador.java
+++ src/model/Treinador.java
@@ -1,40 +1,25 @@
 package model;
 
 public class Treinador {
-    
-    public String nome, usuario, senha;
-    private int genero;
+    private int id;
+    private String nome;
+    private String regiao;
+
+    public Treinador(int id, String nome, String regiao) {
+        this.id = id;
+        this.nome = nome;
+        this.regiao = regiao;
+    }
+
+    public int getId() {
+        return id;
+    }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public String getRegiao() {
+        return regiao;
     }
-
-    public String getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public int getGenero() {
-        return genero;
-    }
-
-    public void setGenero(int genero) {
-        this.genero = genero;
-    }
-    
 }
